38e0cf2c57ebf2b74981a7a762f6552aabe821d6
Marcus <marcus.revaj@gmail.com>
2020-01-21 17:01:32 +0100

    staggered animation added

--- lib/secondPage.dart
+++ lib/secondPage.dart
@@ -22,9 +22,97 @@ class SecondPage extends StatelessWidget {
               child:
                   CircleAvatar(backgroundImage: AssetImage('images/logo.jpg')),
             ),
+            SizedBox(
+              height: 50.0,
+            ),
+            StaggeredAnimation(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class StaggeredAnimation extends StatefulWidget {
+  @override
+  _StaggeredAnimationState createState() => _StaggeredAnimationState();
+}
+
+class _StaggeredAnimationState extends State<StaggeredAnimation>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        _controller.forward();
+        print('tapped');
+      },
+      child: Center(
+        child: Container(
+          child: AnimationWidgetStaggered(controller: _controller),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimationWidgetStaggered extends StatelessWidget {
+  final AnimationController controller;
+  AnimationWidgetStaggered({this.controller})
+      : width = Tween<double>(begin: 55.0, end: 80.0).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.0,
+              0.500,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        height = Tween<double>(begin: 55.0, end: 80.0).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.5,
+              1,
+              curve: Curves.ease,
+            ),
+          ),
+        );
+
+  final Animation<double> width;
+  final Animation<double> height;
+
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    return Container(
+      color: Colors.blue,
+      width: width.value,
+      height: height.value,
+      child: Center(
+        child: Text(
+          'TAP ME!',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: _buildAnimation,
     );
   }
 }
