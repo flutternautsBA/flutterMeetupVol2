40ac05ad0ac2f39295d362d48acae15e9dbee6e4
Marcus <marcus.revaj@gmail.com>
2020-01-21 15:54:54 +0100

    added basic routing to second page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_meetup/secondPage.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
@@ -18,58 +19,74 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.deepPurple.shade900,
           title: Text('My CV'),
         ),
-        body: Container(
-          width: double.infinity,
-          margin: EdgeInsets.all(30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('images/logo.jpg'),
-                radius: 50.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  'My Name',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Comforta',
-                    fontSize: 30.0,
+        body: Builder(
+          builder: (context) => Container(
+            width: double.infinity,
+            margin: EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/logo.jpg'),
+                  radius: 50.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'My Name',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Comforta',
+                      fontSize: 30.0,
+                    ),
                   ),
                 ),
-              ),
-              Text(
-                'My Position',
-                style: GoogleFonts.pacifico(
-                  textStyle: TextStyle(color: Colors.white),
+                Text(
+                  'My Position',
+                  style: GoogleFonts.pacifico(
+                    textStyle: TextStyle(color: Colors.white),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 100.0,
-              ),
-              Divider(
-                color: Colors.white,
-                height: 10.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Chip(
-                      label: Text('I\'m chip'),
-                    ),
-                    Chip(
-                      label: Text('I\'m chip'),
-                    ),
-                    Chip(
-                      label: Text('I\'m chip'),
-                    ),
-                  ],
+                SizedBox(
+                  height: 100.0,
                 ),
-              )
-            ],
+                Divider(
+                  color: Colors.white,
+                  height: 10.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Chip(
+                        label: Text('I\'m chip'),
+                      ),
+                      Chip(
+                        label: Text('I\'m chip'),
+                      ),
+                      Chip(
+                        label: Text('I\'m chip'),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                RaisedButton(
+                  elevation: 20.0,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SecondPage(),
+                        ));
+                  },
+                  child: Text('Let\'s go to next Page!'),
+                )
+              ],
+            ),
           ),
         ),
       ),
